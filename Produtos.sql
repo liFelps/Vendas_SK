93117698191b3f685524e2c926ed777e64bf5555
Felipe Azevedo <felipe.azevedo96@gmail.com>
2024-06-27 11:02:43 -0300

    Update Produtos.sql

--- Produtos.sql
+++ Produtos.sql
@@ -2,7 +2,7 @@ SELECT PRO.CODPROD AS "Produto ID",
        PRO.DESCRPROD AS "Produto",
        GRU.CODPROD AS "Grupo de Produtos ID",
        GRU.DESCRPROD AS "Grupo de Produtos",
-       PRO.CODMARCA AS "Marca ID",
+       PRO.MARCA AS "Marca ID",
        PRO.MARCA AS "Marca "
        
 FROM TGFPRO PRO 
