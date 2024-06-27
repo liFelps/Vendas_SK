efca4e4fc039920052275336ee272b9df9642666
Felipe Azevedo <felipe.azevedo96@gmail.com>
2024-06-27 10:37:52 -0300

    Update Vendedor.sql

--- Vendedor.sql
+++ Vendedor.sql
@@ -2,6 +2,9 @@ SELECT VEN.CODVEND AS "Vendedor ID",
        VEN.APELIDO AS "Vendedor",
        GER.CODVEND AS "Gerente ID",
        GER.APELIDO AS "Gerente",
+       VEN.ATIVO AS "Vendedor Ativo ID",
+       CASE WHEN VEN.ATIVO = 'S' THEN 'Sim'
+       ELSE 'Não' END  AS "Vendedor Ativo",
        CASE WHEN USU.ACCOUNTNOMEFOTO IS NULL THEN 'https://mitra-multitenant-prod.s3.amazonaws.com/tenant_5109/label-images/person.png'
        ELSE USU.ACCOUNTNOMEFOTO END AS "Foto"
 FROM TGFVEN VEN
