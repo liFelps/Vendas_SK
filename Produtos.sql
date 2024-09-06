0f4f9e155bab0bf31c1d171a49c2b203cff663c1
Felipe Azevedo <felipe.azevedo96@gmail.com>
2024-09-06 11:29:12 -0300

    Update Produtos.sql

--- Produtos.sql
+++ Produtos.sql
@@ -2,8 +2,8 @@ SELECT PRO.CODPROD AS "Produto ID",
        PRO.DESCRPROD AS "Produto",
        GRU.CODGRUPOPROD AS "Grupo de Produtos ID",
        GRU.DESCRGRUPOPROD AS "Grupo de Produtos",
-       NVL(PRO.MARCA, 'N/A') AS "Marca ID",
-       NVL(PRO.MARCA, 'Sem marca') AS "Marca "
+       COALESCE(PRO.MARCA, 'N/A') AS "Marca ID",
+       COALESCE(PRO.MARCA, 'Sem marca') AS "Marca "
        
 FROM TGFPRO PRO 
 LEFT JOIN TGFGRU GRU ON PRO.CODGRUPOPROD = GRU.CODGRUPOPROD
